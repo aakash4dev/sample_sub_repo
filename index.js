101a3acfd81e0562547e94d077363e202977c104
aakash4dev <aakash4dev.crypto@gmail.com>
2023-05-10 12:19:11 +0530

    v0.0.2

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log('its sample data')
 const express = require('express')
 const app = express()
 const port = 3000
+// creae hello world server with express
+app.get('/', (req, res) => res.send('Hello World!'))
+// listen to port 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
